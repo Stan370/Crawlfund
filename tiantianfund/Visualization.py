817bb766c0b0f2e578795e85dae12dcb9585bf68
Stan Ke <156306548@qq.com>
2024-08-18 00:22:26 +0800

    fix bugs

--- tiantianfund/Visualization.py
+++ tiantianfund/Visualization.py
@@ -3,62 +3,45 @@ import re
 import json
 import pandas as pd
 import datetime
+from pyecharts.charts import Bar, Line, Grid
+from pyecharts import options as opts
 
-fundCode = 162719
-
-
-class SingleCrawler:
-    def __init__(self,
-                 fund_code: int,
-                 page_range: int = None,
-                 file_name=None):
-        """
-        :param fund_code:  基金代码
-        :param page_range:  获取最大页码数，每页包含20天的数据
-        """
-        self.root_url = 'http://api.fund.eastmoney.com/f10/lsjzt'
+class FundDataVisualizer:
+    def __init__(self, fund_code: int, page_range: int = None, file_name=None):
         self.fund_code = fund_code
-        self.session = requests.session()
         self.page_range = page_range
-        self.file_name = file_name if file_name else '{}.csv'.format(self.fund_code)
+        self.file_name = file_name if file_name else f'{self.fund_code}.csv'
+        self.session = requests.session()
         self.headers = {
             'Host': 'api.fund.eastmoney.com',
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
-            'Referer': 'http://fundf10.eastmoney.com/jjjz_%s.html' % self.fund_code,
+            'User-Agent': 'Mozilla/5.0',
+            'Referer': f'http://fundf10.eastmoney.com/jjjz_{self.fund_code}.html',
         }
+        self.root_url = 'http://api.fund.eastmoney.com/f10/lsjzt'
+        self.bar, self.line, self.grid = self.init_chart()
 
     def fount_info(self):
         search_url = 'https://fundsuggest.eastmoney.com/FundSearch/api/FundSearchAPI.ashx'
-        params = {
-            "callback": "jQuery18309325043269513131_1618730779404",
-            "m": 1,
-            "key": self.fund_code,
-        }
-        res = self.session.get(search_url,
-                               params=params
-                               )
+        params = {"callback": "jQuery18309325043269513131_1618730779404", "m": 1, "key": self.fund_code}
+        res = self.session.get(search_url, params=params)
         try:
             content = self.content_formatter(res.text)
             fields = '，'.join([i['TTYPENAME'] for i in content['Datas'][0]['ZTJJInfo']])
-            print("{:*^30}".format(self.fund_code))
-            print("* 名称：{0:{1}>10} *".format(content['Datas'][0]['NAME'], chr(12288)))
-            print("* 基金类型：{0:{1}>10} *".format(content['Datas'][0]['FundBaseInfo']['FTYPE'], chr(12288)))
-            print("* 基金经理：{0:{1}>10} *".format(content['Datas'][0]['FundBaseInfo']['JJJL'], chr(12288)))
-            print("* 相关领域：{0:{1}>10} *".format(fields, chr(12288)))
-            print("*" * 30)
+            print(f"基金代码: {self.fund_code}")
+            print(f"名称：{content['Datas'][0]['NAME']}")
+            print(f"基金类型：{content['Datas'][0]['FundBaseInfo']['FTYPE']}")
+            print(f"基金经理：{content['Datas'][0]['FundBaseInfo']['JJJL']}")
+            print(f"相关领域：{fields}")
             return True
         except TypeError:
-            print('Fail to pinpoint fund code, {}, please check.'.format(self.fund_code))
+            print(f'Fail to pinpoint fund code, {self.fund_code}, please check.')
 
     @staticmethod
-    def content_formatter(content1):
-        params = re.compile('jQuery.+?\((.*)\)')
-        data = json.loads(params.findall(content1)[0])
+    def content_formatter(content):
+        params = re.compile(r'jQuery.+?\((.*)\)')
+        return json.loads(params.findall(content)[0])
 
-        return data
-
-    def page_data(self,
-                  page_index):
+    def page_data(self, page_index):
         params = {
             'callback': 'jQuery18308909743577296265_1618718938738',
             'fundCode': self.fund_code,
@@ -66,15 +49,13 @@ class SingleCrawler:
             'pageSize': 20,
         }
         res = self.session.get(url=self.root_url, headers=self.headers, params=params)
-        content = self.content_formatter(res.text)
-        return content
+        return self.content_formatter(res.text)
 
     def page_iter(self):
         for page_index in range(self.page_range):
-            item = self.page_data(page_index + 1)
-            yield item
+            yield self.page_data(page_index + 1)
 
-    def get_all(self):
+    def get_all_data(self):
         total_count = float('inf')
         page_index = 0
         while page_index * 20 <= total_count:
@@ -83,82 +64,57 @@ class SingleCrawler:
             total_count = item['TotalCount']
             yield item
 
-    def run(self):
+    def crawl_data(self):
         if self.fount_info():
             df = pd.DataFrame()
             if self.page_range:
                 for data in self.page_iter():
                     df = df.append(data['Data']['LSJZList'], ignore_index=True)
-                    print("\r{:*^30}".format(' DOWNLOADING '), end='')
+                    print("Downloading data...", end='\r')
             else:
-                for data in self.get_all():
+                for data in self.get_all_data():
                     df = df.append(data['Data']['LSJZList'], ignore_index=True)
-                    print("\r{:*^30}".format(' DOWNLOADING '), end='')
+                    print("Downloading data...", end='\r')
             df.to_csv(self.file_name)
-            print("\r{:*^30}".format(' WORK DONE '))
-            print("{:*^30}".format(' FILE NAME '))
-            print("*{:^28}*".format(self.file_name))
-            print("*" * 30)
-
-
-def load_data(file):
-    df = pd.read_csv(file)
-    # 转换成时间格式
-    df['FSRQ'] = pd.to_datetime(df['FSRQ'])
-    # 按时间排序
-    df.sort_values(by=['FSRQ'], inplace=True)
-    # 日增长率填充0
-    df['JZZZL'].fillna(0, inplace=True)
-    max_date = df['FSRQ'].max()
-    min_date = datetime.datetime.strptime('2021-1-1', '%Y-%m-%d')
-    df = df[df['FSRQ'].isin(pd.date_range(start=min_date, end=max_date))]
-
-    data_x, data_y_day, data_y_total = [], [], []
-    money = 10000
-    for _, row in df.iterrows():
-        data_x.append(pd.to_datetime(row['FSRQ']).strftime('%Y-%m-%d'))
-        money_diff = money * (float(row['JZZZL']) / 100)
-        money = money + money_diff
-        # print(diff_rate, money, money_diff)
-        data_y_day.append(round(money_diff, 2))
-        data_y_total.append(round(money, 0))
-    return data_x, data_y_day, data_y_total
-
-
-from pyecharts.charts import *
-from pyecharts import options as opts
-
-
-class DataViz:
-    def __init__(self,
-                 file,
-                 name):
-        self.file = file
-        self.name = name
-        self.bar, self.line, self.grid = self.init_char()
-        self.data_x, self.data_y_day, self.data_y_total = load_data(file)
-        print(self.data_x, self.data_y_day, self.data_y_total)
+            print(f"Data saved to {self.file_name}")
+            return self.file_name
 
     @staticmethod
-    def init_char():
+    def load_data(file):
+        df = pd.read_csv(file)
+        df['FSRQ'] = pd.to_datetime(df['FSRQ'])
+        df.sort_values(by=['FSRQ'], inplace=True)
+        df['JZZZL'].fillna(0, inplace=True)
+        max_date = df['FSRQ'].max()
+        min_date = datetime.datetime.strptime('2021-1-1', '%Y-%m-%d')
+        df = df[df['FSRQ'].isin(pd.date_range(start=min_date, end=max_date))]
+        data_x, data_y_day, data_y_total = [], [], []
+        money = 10000
+        for _, row in df.iterrows():
+            data_x.append(pd.to_datetime(row['FSRQ']).strftime('%Y-%m-%d'))
+            money_diff = money * (float(row['JZZZL']) / 100)
+            money += money_diff
+            data_y_day.append(round(money_diff, 2))
+            data_y_total.append(round(money, 0))
+        return data_x, data_y_day, data_y_total
+
+    def init_chart(self):
         bar = Bar()
         line = Line()
         grid = Grid(
             init_opts=opts.InitOpts(
                 theme='white',
-                # bg_color=JsCode(bg_color_js),
                 width='1600px',
                 height='900px'
-
             )
         )
         return bar, line, grid
 
-    def bar_viz(self):
-        self.bar.add_xaxis(self.data_x)
+    def bar_viz(self, data_x, data_y_day):
+        self.bar.add_xaxis(data_x)
         self.bar.add_yaxis(
-            self.name,
-            self.data_y_day,
+            self.fund_code,
+            data_y_day,
             itemstyle_opts={
                 "normal": {
                     'shadowBlur': 10,
@@ -178,14 +134,11 @@ class DataViz:
                 is_show=False,
                 is_piecewise=True,
                 pieces=[{"max": 0, "color": 'green'},
-                        {"min": 1,
-                         "color": 'red'},
-                        ]
+                        {"min": 1, "color": 'red'},]
             ),
             yaxis_opts=opts.AxisOpts(
                 name='日收益/元',
                 type_="value",
-                # is_scale=True,
                 splitline_opts=opts.SplitLineOpts(
                     is_show=True,
                     linestyle_opts=opts.LineStyleOpts(
@@ -201,11 +154,11 @@ class DataViz:
         )
         return self
 
-    def line_viz(self):
-        self.line.add_xaxis(self.data_x)
+    def line_viz(self, data_x, data_y_total):
+        self.line.add_xaxis(data_x)
         self.line.add_yaxis(
-            self.name,
-            self.data_y_total,
+            self.fund_code,
+            data_y_total,
             is_symbol_show=True,
             symbol='circle',
             symbol_size=1,
@@ -214,9 +167,7 @@ class DataViz:
                             'shadowColor': 'rgba(0, 0, 0, 0.5)',
                             'shadowOffsetY': 10,
                             'shadowOffsetX': 10,
-                            'width': 2,
-                            'curve': 0.1,
-                            },
+                            'width': 2,},
             tooltip_opts=opts.TooltipOpts(is_show=False),
             label_opts=opts.LabelOpts(is_show=False)
         )
@@ -225,9 +176,7 @@ class DataViz:
                 is_show=False,
                 is_piecewise=True,
                 pieces=[{"max": 0, "color": 'green'},
-                        {"min": 1,
-                         "color": 'red'},
-                        ]
+                        {"min": 1, "color": 'red'},]
             ),
             yaxis_opts=opts.AxisOpts(
                 name='持仓/元', type_="value", is_scale=True,
@@ -240,9 +189,7 @@ class DataViz:
                 splitline_opts=opts.SplitLineOpts(is_show=True,
                                                   linestyle_opts=opts.LineStyleOpts(
                                                       type_='dashed'))),
-            legend_opts=opts.LegendOpts(
-                is_show=False
-            ),
+            legend_opts=opts.LegendOpts(is_show=False),
             title_opts=opts.TitleOpts(
                 title="1W元本金日收益趋势",
                 pos_left='center',
@@ -250,94 +197,43 @@ class DataViz:
                     color='#08A05C',
                     font_size=20,
                     font_weight='bold'),
-                subtitle='{}'.format(self.name),
+                subtitle=f'{self.fund_code}',
             ),
             graphic_opts=[
                 opts.GraphicGroup(
                     graphic_item=opts.GraphicItem(id_='1', left="250px", top="250px"),
                     children=[
                         opts.GraphicRect(
-                            graphic_item=opts.GraphicItem(
-                                left="center", top="center", z=1
-                            ),
-                            graphic_shape_opts=opts.GraphicShapeOpts(
-                                width=250, height=120
-                            ),
+                            graphic_item=opts.GraphicItem(left="center", top="center", z=1),
+                            graphic_shape_opts=opts.GraphicShapeOpts(width=250, height=120),
                             graphic_basicstyle_opts=opts.GraphicBasicStyleOpts(
-                                # 颜色配置，这里设置为黑色，透明度为0.5
-                                fill="rgba(0,0,0,0.5)",
-                                line_width=4,
-                                stroke="#000",
-                            ),
+                                fill="rgba(0,0,0,0.5)", line_width=4, stroke="#000",),
                         ),
                         opts.GraphicText(
-                            graphic_item=opts.GraphicItem(
-                                left="center", top="center", z=100
-                            ),
+                            graphic_item=opts.GraphicItem(left="center", top="center", z=100),
                             graphic_textstyle_opts=opts.GraphicTextStyleOpts(
-                                # 要显示的文本
-                                text='{}\n\n截止日期：{}\n\n持有收益：{}元\n\n持有收益率：{:.2%}'.format(
-                                    self.name,
-                                    self.data_x[-1],
-                                    self.data_y_total[-1] - 10000,
-                                    self.data_y_total[-1] / 10000 - 1),
-                                font="bold 14px Microsoft YaHei",
-                                graphic_basicstyle_opts=opts.GraphicBasicStyleOpts(
-                                    fill="#fff"
-                                ),
-                            ),
+                                text=f'{self.fund_code}\n\n截止日期：{data_x[-1]}\n\n持有收益：{data_y_total[-1] - 10000}元
+                                \n\n收益率：{(data_y_total[-1] - 10000) / 10000 * 100}%',
+                                font="bold 20px Microsoft YaHei",
+                                graphic_basicstyle_opts=opts.GraphicBasicStyleOpts(fill="#fff")),
                         )
-                    ],
-                ), ]
+                    ])
+            ]
         )
-        self.line.set_series_opts(
-            label_opts=opts.LabelOpts(is_show=False),
-            markline_opts=opts.MarkLineOpts(
-                data=[
-                    opts.MarkLineItem(type_="min", name="最小值"),
-                    opts.MarkLineItem(type_="max", name="最大值"),
-                ],
-                label_opts=opts.LabelOpts(is_show=True, position='left', formatter='{b}：\n{c}元'),
-            ),
-            markpoint_opts=opts.MarkPointOpts(
-                data=[
-                    opts.MarkPointItem(
-                        name="收益",
-                        coord=[self.data_x[-1], self.data_y_total[-1]],
-                        value=int(self.data_y_total[-1]),
-                        symbol_size=[80, 50]
-                    )
-                ]
-            )
-        )
+        return self
 
-    def run(self):
-        self.line_viz()
-        self.bar_viz()
-        self.grid.add(
-            self.line,
-            is_control_axis_index=False,
-            grid_opts=opts.GridOpts(
-                pos_left='7%',
-                pos_right='7%',
-                pos_top='10%',
-                pos_bottom='50%')
-        )
+    def render(self):
+        self.grid.add(self.bar, grid_opts=opts.GridOpts(pos_right="55%"))
+        self.grid.add(self.line, grid_opts=opts.GridOpts(pos_left="50%"))
+        self.grid.render(f'{self.fund_code}.html')
 
-        self.grid.add(
-            self.bar,
-            is_control_axis_index=False,
-            grid_opts=opts.GridOpts(
-                pos_left='7%',
-                pos_right='7%',
-                pos_top='60%',
-                pos_bottom='5%')
-        )
-        self.grid.render(self.file.replace('csv', 'html'))
+    def visualize(self):
+        file = self.crawl_data()
+        data_x, data_y_day, data_y_total = self.load_data(file)
+        self.bar_viz(data_x, data_y_day)
+        self.line_viz(data_x, data_y_total)
+        self.render()
 
-
-if __name__ == "__main__":
-    c = SingleCrawler(fundCode)
-    c.run()
-    vis = DataViz(file=('162719.csv'), name='广发道琼斯石油指数人民币A')
-    vis.run()
+# Example of usage
+fund_visualizer = FundDataVisualizer(320007, page_range=1)
+fund_visualizer.visualize()
